4a39b6f0ef07fcf3646d1e1c5bf2d7818bdd3ba6
justGyga <iwantdrink229@gmail.com>
2024-08-17 10:26:41 +0600

    feature(currency): getRatesByOwnCurrency

--- modules/currency/service.js
+++ modules/currency/service.js
@@ -9,13 +9,28 @@ class CurrencyService {
         if (areCurrenciesExist != 2) return false;
         const APIstring = `${process.env.CURRENCY_API_HOST}/${process.env.CURRENCY_API_KEY}/pair/${base}/${target}`;
         const response = await axios.get(APIstring);
-        console.log(response);
         const { data } = response;
         if (data.result != "success") {
             await redis.set(`${NON_CONVERT}:user:${userId}`, JSON.stringify({ amount, base, target }), "EX", process.env.CONVERT_TTL);
         }
         const { conversation_rate: rate } = data;
         return amount * rate;
+    }
+
+    async getRatesByOwnCurrency(userCurrency, targets = []) {
+        const base = userCurrency.code;
+        const targetsCodes = (await Currency.findAll({ where: { code: targets }, attributes: ["code"] })).map(({ code }) => code);
+        if (!targetsCodes.length) return [false, false];
+        const APIstring = `${process.env.CURRENCY_API_HOST}/${process.env.CURRENCY_API_KEY}/latest/${base}`;
+        const response = await axios.get(APIstring);
+        const { data } = response;
+        if (data.result != "success") return [false, false];
+        const { conversion_rates: rates } = data;
+        const resultRates = {};
+        targetsCodes.forEach((code) => {
+            resultRates[code] = rates[code];
+        });
+        return resultRates;
     }
 }
 export default CurrencyService;
