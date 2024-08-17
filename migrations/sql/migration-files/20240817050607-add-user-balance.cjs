32119ac58cb37775252a6c75a39125f945f12e12
justGyga <iwantdrink229@gmail.com>
2024-08-17 11:08:25 +0600

    migrate(user): add balance in user table

---
+++ migrations/sql/migration-files/20240817050607-add-user-balance.cjs
@@ -0,0 +1,21 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        /**
+         * Add altering commands here.
+         *
+         * Example:
+         * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+         */
+        await queryInterface.addColumn("users", "balance", { type: Sequelize.FLOAT, defaultValue: 0 });
+    },
+
+    async down(queryInterface, Sequelize) {
+        /**
+         * Add reverting commands here.
+         *
+         * Example:
+         * await queryInterface.dropTable('users');
+         */
+    }
+};
